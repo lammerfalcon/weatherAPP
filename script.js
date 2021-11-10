e904a264e63b179d482f107c034ea91d6edbe788
Rodion Gavrilov <rodiongavrilov@gmail.com>
2021-11-10 14:29:19 +0300

    [Fix] adaptive and new API

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
+
+
+
 window.addEventListener('load', () => {
     let long;
     let lat;
     let temperatureDescription = document.querySelector('.temperature-description');
-    let temperatureDegree = document.querySelector('.temperature-degree');
+    let temperatureCurrent = document.querySelector('.temperature-current');
+    let morning = document.querySelector('.morning-degree');
+    let afternoon = document.querySelector('.afternoon-degree');
+    let evening = document.querySelector('.evening-degree');
+    let night = document.querySelector('.night-degree');
     let locationTimezone = document.querySelector('.location-timezone');
-    let belgorod = document.querySelector('.belgorod');
-    let rome = document.querySelector('.rome');
-    let city = belgorod.textContent;
+    // let belgorod = document.querySelector('.belgorod');
+    // let rome = document.querySelector('.rome');
+    // let city = belgorod.textContent;
 
 
-    rome.addEventListener('click', function () {
-        city = rome.textContent;
-        console.log(city);
-    });
+    // rome.addEventListener('click', function () {
+    //     city = rome.textContent;
+    //     console.log(city);
+    // });
 
 
     if (navigator.geolocation) {
@@ -20,52 +27,45 @@ window.addEventListener('load', () => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
             let api;
-            api = `https://community-open-weather-map.p.rapidapi.com/weather?q=${city}&lat=${lat}&lon=${long}&lang=null&units=metric`
+            api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=metric&lang=ru&appid=ee9efec6c56ee11c9fb295a760e5aba6`;
+
+            let geo = `https://open.mapquestapi.com/geocoding/v1/reverse?key=S9JzRzwJ3IevzgsLvU7etpaNxwyZoj3u&location=${lat},${long}`
+
+
+
+
+            fetch(geo)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    const { adminArea5 } = data.results[0].locations[0];
+                    locationTimezone.textContent = adminArea5;
+                });
+
 
             fetch(api, {
                 "method": "GET",
-                "headers": {
-                    "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-                    "x-rapidapi-key": "35d88a033cmsh92ee3ea148f7562p13aa39jsnf6a332875aa9"
-                }
+
             })
                 .then(response => {
                     return response.json();
                 })
                 .then(data => {
                     console.log(data);
-                    const { temp, feels } = data.main;
-                    const { name } = data;
-                    const { icon } = data.weather[0].icon;
-                    const { main } = data.weather[0].main;
+                    morning.textContent = Math.round(data.hourly[0].temp);
+                    afternoon.textContent = Math.round(data.hourly[6].temp);
+                    evening.textContent = Math.round(data.hourly[12].temp);
+                    night.textContent = Math.round(data.hourly[16].temp);
+                    const { icon } = data.current.weather[0];
                     //Set DOM Elements from the API
-                    temperatureDegree.textContent = temp;
-                    locationTimezone.textContent = name;
-                    temperatureDescription.textContent = data.weather[0].description;
-                    setIcons(icon, document.querySelector('.icon'))
+                    temperatureCurrent.textContent = Math.round(data.current.temp);
+                    temperatureDescription.textContent = data.current.weather[0].description;
+                    wicon.src = 'https://openweathermap.org/img/wn/' + icon + '@2x.png';
                 })
         })
     }
 
-    function setIcons(icon, iconID) {
-        const skycons = new Skycons({ color: "white" });
-        let currentIcon;
-        if (temperatureDescription.textContent === "overcast clouds") {
-            currentIcon = Skycons.CLOUDY;
-        } else if (temperatureDescription.textContent === "light rain") {
-            currentIcon = Skycons.RAIN;
-        } else if (temperatureDescription.textContent === "clear sky") {
-            currentIcon = Skycons.CLEAR_DAY;
-        } else if (temperatureDescription.textContent === "broken clouds") {
-            currentIcon = Skycons.PARTLY_CLOUDY_DAY;
-
-        } else if (temperatureDescription.textContent === "fog") {
-            currentIcon = Skycons.FOG;
-        }
-
-        skycons.play();
-        return skycons.set(iconID, currentIcon);
-    }
 
 
 })
